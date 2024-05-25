330b9d1e095cd6905ba8e82ac21f47eba90e6544
Bhumit Jain <bhumitjain942@gmail.com>
2024-05-25 19:04:39 +0530

    react assignment 2 submission

---
+++ React Assignmet -2/a2p1/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Posts from "./components/Posts";
+import Users from "./components/Users";
+
+export default function App() {
+  return (
+    <>
+    <h1>Assignemt</h1>
+      <Users />
+      <Posts />
+    </>
+  );
+}
