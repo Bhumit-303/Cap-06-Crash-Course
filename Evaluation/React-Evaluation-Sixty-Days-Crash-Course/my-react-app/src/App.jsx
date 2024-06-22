b6656e16395dfc84ee5f0ae00443c06ac9316b1d
Bhumit Jain <bhumitjain942@gmail.com>
2024-06-22 13:44:25 +0530

    evaluation

---
+++ Evaluation/React-Evaluation-Sixty-Days-Crash-Course/my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import AllRouts from './component/AllRouts'
+import Navbar from './component/navbar'
+
+function App() {
+  return (
+    <div>
+     <Navbar/>
+     <AllRouts/>
+    </div>
+  )
+}
+
+export default App
---
+++ Evaluation/React-Evaluation-Sixty-Days-Crash-Course/my-react-app/src/Context/Authcontext.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { createContext,useState } from 'react'
+
+export const Authcontext = createContext()
+export default function AuthcontextProvider({children}) {
+const [authState, setauthState] = useState({ isAuthenticated: false, token: null, email: null })
+console.log(authState)
+const login = (token,email)=>{
+    setauthState({ isAuthenticated: true, token: token, email: email })
+}
+
+const logOut = ()=>{
+    setauthState({ isAuthenticated: false, token: null, email: null })
+}
+
+  return (
+   <Authcontext.Provider value={{authState,login,logOut}}>
+    {children}
+   </Authcontext.Provider>
+  )
+}
+
+// auth context part
